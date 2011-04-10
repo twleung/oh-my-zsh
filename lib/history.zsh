00ff58db501d4499691716f9d35eb7e7c8a6d564
Ted Leung <twl@sauria.com>
2011-04-10 12:45:48 -0700

    Stop sharing history across shells

--- lib/history.zsh
+++ lib/history.zsh
@@ -4,15 +4,15 @@ HISTSIZE=10000
 SAVEHIST=10000
 
 setopt hist_ignore_dups # ignore duplication command history list
-setopt share_history # share command history data
+#setopt share_history # share command history data
 
 setopt hist_verify
-setopt inc_append_history
+#setopt inc_append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_space
 
-setopt SHARE_HISTORY
+#setopt SHARE_HISTORY
 setopt APPEND_HISTORY
 
 # TWL
