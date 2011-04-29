496421c72c68aebef616ce7c0b5b5d389519b48a
Ted Leung <twl@sauria.com>
2011-04-29 00:55:29 -0700

    add completion for autossh / tscreen - real autocompletion is in a ZPATH function

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -42,6 +42,7 @@ zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm 
 if [ -f ~/.ssh/known_hosts ]; then
   zstyle ':completion:*' hosts $( sed 's/[, ].*$//' $HOME/.ssh/known_hosts )
   zstyle ':completion:*:*:(ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
+#  zstyle ':completion:*:*:(autossh|tscreen):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
 fi
 
 setopt hashcmds hashdirs hashlistall
